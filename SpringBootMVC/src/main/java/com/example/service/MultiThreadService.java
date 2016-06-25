2bf9ec90e476b4e0330d5ac89c4ce30c493ee06e
solomonl <cppbomb@qq.com>
2016-06-25 11:17:32 +0800

    Add semaphore implementation.

--- SpringBootMVC/src/main/java/com/example/service/MultiThreadService.java
+++ SpringBootMVC/src/main/java/com/example/service/MultiThreadService.java
@@ -95,8 +95,6 @@ public class MultiThreadService {
         });
         //它可以期待一个返回值是completableFuture，类似promise里接promise
         downloading.thenCompose(this::strLen);
-
-
     }
 
     private CompletableFuture<Integer> strLen(String s) {
