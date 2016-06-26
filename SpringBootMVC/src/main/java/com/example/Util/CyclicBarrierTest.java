bed4edc5c89202514f3d1eb342e5b4e5a02ccf94
solomonl <cppbomb@qq.com>
2016-06-26 18:43:29 +0800

    增加注解

--- SpringBootMVC/src/main/java/com/example/Util/CyclicBarrierTest.java
+++ SpringBootMVC/src/main/java/com/example/Util/CyclicBarrierTest.java
@@ -61,6 +61,13 @@ public class CyclicBarrierTest {
             try {
                 log.info("T3 meet the barrier");
                 //这里有一个bug很有意思，如果使用了c1，会使t1，t2继续，t3却不能继续
+                //c1是可以被继续使用的，所以是可循环的。
+                /*
+                * CyclicBarrier和CountDownLatch的区别
+
+CountDownLatch的计数器只能使用一次。而CyclicBarrier的计数器可以使用reset() 方法重置。所以CyclicBarrier能处理更为复杂的业务场景，比如如果计算发生错误，可以重置计数器，并让线程们重新执行一次。
+CyclicBarrier还提供其他有用的方法，比如getNumberWaiting方法可以获得CyclicBarrier阻塞的线程数量。isBroken方法用来知道阻塞的线程是否被中断。比如以下代码执行完之后会返回true。
+                * */
                 c2.await();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
