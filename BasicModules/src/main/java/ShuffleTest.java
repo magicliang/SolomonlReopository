4417ae498c0c567b13aff0e3459c2f116afe96df
solomonl <cppbomb@qq.com>
2016-08-03 22:45:37 +0800

    增加洗牌程序的注释

--- BasicModules/src/main/java/ShuffleTest.java
+++ BasicModules/src/main/java/ShuffleTest.java
@@ -14,6 +14,7 @@ public class ShuffleTest {
 
     private static void shuffle(int[] array){
         //don't shuffle the first element
+        //从后倒数回来。可以证明每一个点只在前方的子集里交换的话，不会再与后方的数交换，则交换的概率始终为 1/n。
         for(int i = array.length - 1; i > 0; i--){
             int j = random.nextInt(i+1);//Use an upper bound.
             int temp = array[j];
