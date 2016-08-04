b493632effa8d25af73226ff8ece0ed93591c6eb
solomonl <cppbomb@qq.com>
2016-08-04 08:00:27 +0800

    增加洗牌程序的注释

--- BasicModules/src/main/java/ShuffleTest.java
+++ BasicModules/src/main/java/ShuffleTest.java
@@ -11,7 +11,7 @@ public class ShuffleTest {
         shuffle(array);
         System.out.println(array);
    }
-
+    //所谓的yates算法，最基本的洗牌算法
     private static void shuffle(int[] array){
         //don't shuffle the first element
         //从后倒数回来。可以证明每一个点只在前方的子集里交换的话，不会再与后方的数交换，则交换的概率始终为 1/n。
