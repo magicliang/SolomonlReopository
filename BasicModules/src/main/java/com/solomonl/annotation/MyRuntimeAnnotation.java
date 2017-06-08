9b44e722351bd4ccaf9046c8b7fb1b240272681a
liangchuan <liangchuan@didichuxing.com>
2017-06-08 20:43:01 +0800

    加注解的例子

---
+++ BasicModules/src/main/java/com/solomonl/annotation/MyRuntimeAnnotation.java
@@ -0,0 +1,35 @@
+package com.solomonl.annotation;
+
+import java.awt.event.ActionListener;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * 这是运行时注解。编译时注解最好用 source 的retention type 加上 Annotation Processing Tool来在 javac时进行处理。具体可以谷歌搜索。
+ * Created by liangchuan on 2017/6/8.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface MyRuntimeAnnotation {
+
+    String name() default "lc";
+    int age() default 1;
+
+    // 一个相关的用法，可以把某些方法目标 method 标示出来，例如。这也是为什么我们能在很多框架中看到 xx.class 的配置文件的原因。
+    Class<? extends ActionListener> listerner();
+
+    /**
+     * 真正使用起来的代码大概是：
+     * annotation = object.getClass.getAnnotation(MyRuntimeAnnotation);
+     * Class clazz = annotation.listener();
+     * Listerner ls = clazz.newInstance();
+     * object.addListener(ls)
+     */
+
+
+
+}
---
+++ BasicModules/src/main/java/com/solomonl/annotation/TestAnnotation.java
@@ -0,0 +1,16 @@
+package com.solomonl.annotation;
+
+/**
+ * Created by liangchuan on 2017/6/8.
+ */
+@MyRuntimeAnnotation
+public class TestAnnotation {
+
+    public static void main(String[] args) {
+        MyRuntimeAnnotation annotation = TestAnnotation.class.getAnnotation(MyRuntimeAnnotation.class);
+
+        System.out.println("Get name: " + annotation.name());
+        System.out.println("Get age: " + annotation.age());
+
+    }
+}
