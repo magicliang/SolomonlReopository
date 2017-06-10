b78f8790b8b6ea7a64c516b0f0f3c4a8e2a049ca
Magicliang <magicliang@qq.com>
2017-06-10 13:41:52 +0800

    增加一个 checked exception 的实验

---
+++ BasicModules/src/main/java/com/solomonl/exception/MyException.java
@@ -0,0 +1,8 @@
+package com.solomonl.exception;
+
+
+/**
+ * Created by LC on 2017/6/10.
+ */
+public class MyException extends Exception {
+}
---
+++ BasicModules/src/main/java/com/solomonl/util/FruiteEnum.java
@@ -0,0 +1,7 @@
+package com.solomonl.util;
+
+/**
+ * Created by LC on 2017/3/23.
+ */
+public enum FruiteEnum {
+}
