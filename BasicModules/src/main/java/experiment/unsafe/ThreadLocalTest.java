40ef3c4574b4da73e5800d054422e6fa81c9a106
Magicliang <magicliang@qq.com>
2017-07-02 16:51:44 +0800

    加入一个 threadlocal 的例子。

--- BasicModules/src/main/java/experiment/unsafe/ThreadLocalTest.java
+++ BasicModules/src/main/java/experiment/unsafe/ThreadLocalTest.java
@@ -6,9 +6,10 @@ import java.util.stream.IntStream;
  * 一个从 SO 上看来的问题。如果想要跨线程传递变量，应该把“main”当做局部变量通过闭包的方式设值。
  * ThreadLocal本身就是为了线程隔离准备的。除了初始值以外，最好不要试图跨线程共享。
  * 或者考虑使用  InheritableThreadLocal。它会天然继承父线程的threadlocal 的值。
+ * 也可以考虑用闭包的方式对initValue赋值。
  * Created by LC on 2017/7/2.
  */
-public class threadlocal {
+public class ThreadLocalTest {
     public static void main(String[] args) {
         ThreadContext.set("MAIN");
         System.out.printf("Main Thread: %s\n", ThreadContext.get());
