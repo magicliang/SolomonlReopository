b46f33c15894ea43ce29b840e78a26b5964eb403
Magicliang <magicliang@qq.com>
2018-07-11 21:56:49 +0800

    提交一个 TypeReference 问题

--- BasicModules/src/main/java/experiment/jackson/TypeReferenceExample.java
+++ BasicModules/src/main/java/experiment/jackson/TypeReferenceExample.java
@@ -1,57 +1,12 @@
 package experiment.jackson;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * 这个例子证明了一件事，jackson 无法用 TypeReference 转化哪怕一级泛型成员
+ * jackson 的typereference无法推导参数化类型，看起来能推导出来，取实际类型数据的时候，还是linkhashmap。
  */
 public class TypeReferenceExample {
-    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static <T> Entity<T> deserialize(String message) throws IOException {
-
-        /**
-         * 少了这个壳类型就不行。这个壳如果不存在，则 jackson会选用 LinkHashMap作壳
-         */
-        Entity<T> entity = OBJECT_MAPPER.readValue(message, new TypeReference<Entity<T>>() {
-        });
-        return entity;
-    }
-
-    public static <T> String serialize(T data) throws JsonProcessingException {
-        return OBJECT_MAPPER.writeValueAsString(data);
-    }
-
-    public static void main(String[] args) throws IOException {
-        TypeReferenceExample typeReferenceExample = new TypeReferenceExample();
-        Entity<List<List<String>>> entity = new Entity<>();
-        List<List<String>> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
-        list1.add(list2);
-
-        for (int i = 0; i < 10; i++) {
-            list2.add("" + i);
-        }
-
-        entity.setData(list1);
-
-        String msg = serialize(entity);
-
-        System.out.println("message is: " + msg);
-
-        // 使用成员方法来使用类型见证帮助推导也于事无补。
-        Entity<List<String>> entity2 = TypeReferenceExample.deserialize(msg);
-
-        System.out.println("entity2 is:" + entity2);
-
-    }
 }
 
 @Data
